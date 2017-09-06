11a77a21815269011db5c497ac1ad4e902d58df1
ma1m2 <ma1m2@mail.ru>
2017-09-06 20:08:53 +0300

    Новая схема FF52esr

--- testselenium/java-example/src/test/java/ru/stqa/selenium/FFesr52.java
+++ testselenium/java-example/src/test/java/ru/stqa/selenium/FFesr52.java
@@ -1,10 +1,18 @@
 package ru.stqa.selenium;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class FFesr52 {
   private WebDriver driver;
@@ -12,9 +20,24 @@ public class FFesr52 {
 
   @Before
   public void start(){
+    //driver = new ChromeDriver();
     DesiredCapabilities caps = new DesiredCapabilities();
-    caps.setCapability(FirefoxDriver.MARIONETTE, false);
+    caps.setCapability(FirefoxDriver.MARIONETTE, true);
     driver = new FirefoxDriver(caps);
+    System.out.println(((HasCapabilities)driver).getCapabilities());
+    wait = new  WebDriverWait(driver,10);
+  }
 
+  @Test
+  public void test(){
+    driver.get("https://www.google.ru");
+    driver.findElement(By.name("q")).sendKeys("webdriver" + Keys.ENTER);
+    wait.until(titleIs("webdriver - Поиск в Google"));
+  }
+
+  @After
+  public void stop(){
+    driver.quit();
+    driver = null;
   }
 }
