cbedf9bd4b21ffd7aa53fc7b54fec6a297963ff9
ma1m2 <ma1m2@mail.ru>
2017-09-06 21:04:25 +0300

    esr52 старая схема более явно

---
+++ testselenium/java-example/src/test/java/ru/stqa/onebrowther/FFesr52old.java
@@ -0,0 +1,38 @@
+package ru.stqa.onebrowther;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class FFesr52old {
+  private WebDriver driver;
+  private WebDriverWait wait;
+
+  @Before
+  public void start(){
+    FirefoxOptions options = new FirefoxOptions().setLegacy(true);// старая схема более явно
+    driver = new FirefoxDriver(options);
+    wait = new  WebDriverWait(driver,10);
+  }
+
+  @Test
+  public void test(){
+    driver.get("https://www.google.ru");
+    driver.findElement(By.name("q")).sendKeys("webdriver" + Keys.ENTER);
+    wait.until(titleIs("webdriver - Поиск в Google"));
+  }
+
+  @After
+  public void stop(){
+    driver.quit();
+    driver = null;
+  }
+}
